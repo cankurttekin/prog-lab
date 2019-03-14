a48d82a9e42425f7d2d7ceee7bff862a20a2a4b7
cankurttekin <cankurttekin@gmail.com>
2019-03-14 19:23:35 +0300

    Update ders1.py

--- ders1.py
+++ ders1.py
@@ -1,36 +1,38 @@
-#Listeye Random sayılar ekleme
 import random
-def generate_an_array(n):
-    my_array=[]
-    for  i in range(n):
-        s=random.randint(0,100)
-        my_array.append(s)
-    return my_array
-my_arr_1=generate_an_array(10)
-print(my_arr_1) 
+def generate_array (n):
+    #Generate an array with n elements.
+    arr = []
+    for i in range (n):
+        arr.append(random.randint(0,100))
+    return arr
 
-#Listenin içinde aranan sayının olup olmadığını kontrol eden kod
-def my_search_c(array_2,item):
-  found=False
-  indis =-1
-  step=0
-  for i in range(len(array_2)):
-    step+=1
-    #if(i==item):
-      #found =True
-    if (array_2[i]==item):
-      found=True
-      indis=i
-      break
-  return found,indis,step
+##################################
 
-#Bubble Sort sıralama kodu
-for i in range (len(my_arr_1)-1,0,-1):
-  for j in range(i):
-    if(my_arr_1[j]>my_arr_1[j+1]):
-      t=my_arr_1[j]
-      my_arr_1[j]=my_arr_1[j+1]
-      my_arr_1[j+1]=t
+def bubble_sort(array):
+    #Sorts an array with Bubble Sort Algorithm
+    for i in range(len(array)):
+        for j in range(len(array)-1):
+            if array[j]>array[j+1]:
+                array[j+1],array[j] = array[j],array[j+1]
+    return array
 
-print(my_arr_1)
-print(my_search_c(my_arr_1,68))
+##################################
+
+def selection_sort(array):
+    #Sorts an array with Selection Sort Algorithm
+    for i in range (len(array)-1):
+        min = i
+        for j in range (i+1,len(array)):
+            if (array[j] < array[min]):
+                min = j
+        array[i],array[min] = array[min],array[i]
+
+##################################
+
+def search_in_array(array, item):
+    #Checks if item in array
+    for i in range(len(array)):
+        if array[i]==item:
+            return i
+    else:
+        return "NOT FOUND"
